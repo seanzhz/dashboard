58c01544b6ccd34ad5946518a5c231fe72804b74
seanzhz <zhz11433012@gmail.com>
2025-05-06 13:10:11 +1000

    Change some text..

--- src/views/Unauth.jsx
+++ src/views/Unauth.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Unauth(props) {
+function Unauth() {
     return (
         <div>
-            <h1>Unauthorized</h1>
+            <h1>Unauthorized, please log in</h1>
         </div>
     );
 }
